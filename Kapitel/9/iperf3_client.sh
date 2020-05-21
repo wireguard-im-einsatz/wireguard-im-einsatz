7dd890db1dc54d6fb2f0bf06e28fd2444f2c39c9
WireGuard im Einsatz <52246165+wireguard-im-einsatz@users.noreply.github.com>
2020-05-21 19:58:31 +0200

    2nd edition

--- Kapitel/9/iperf3_client.sh
+++ Kapitel/9/iperf3_client.sh
@@ -3,14 +3,14 @@
 INTERFACE=eth0
 PEER=10.6.3.1   # WireGuard tunnel
 
-for MTU in 1420 1280 1024 512 256 128 88 ; do
+for MTU in 1500 1420 1280 1024 512 256 128 88 ; do
   echo "*** Benchmarking with MTU ${MTU} Bytes"
-  ifconfig ${INTERFACE} mtu ${MTU}
+  ip link set ${INTERFACE} mtu ${MTU}
   sleep 2
   iperf3 --client ${PEER} --time 60 --interval 60 --window 128K --format m | grep bit
 done
 
 # reset interface MTU
-sudo ifconfig ${INTERFACE} down
-sudo ifconfig ${INTERFACE} mtu 1500
-sudo ifconfig ${INTERFACE} up
+ip link set ${INTERFACE} down
+ip link set ${INTERFACE} mtu 1500
+ip link set ${INTERFACE} up
